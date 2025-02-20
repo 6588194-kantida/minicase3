e2af8dd2372691bf43961bb71fda75c53d625fde
kantida <109674315+6588194-kantida@users.noreply.github.com>
2025-02-20 12:34:50 +0700

    Add files via upload

---
+++ HolidayTravelVehicles/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Vehicle car = new Vehicle("A574F","BMW Y Series","2022","BMW Company",2300000.50f);
+        car.VehicleInfo();
+    }
+}
+
