9c14f467d5b15ee9a1c9ea75d346cce4bf6b6742
kantida <109674315+6588194-kantida@users.noreply.github.com>
2025-02-20 12:33:43 +0700

    Delete Minicase directory

--- Minicase/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Vehicle car = new Vehicle("A574F","BMW Y Series","2022","BMW Company",2300000.50f);
-        car.VehicleInfo();
-    }
-}
-
